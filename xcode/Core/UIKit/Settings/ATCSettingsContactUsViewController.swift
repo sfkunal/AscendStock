695348cee10218105da503a59f27942b368d3b33
samuellee1224 <68315447+samuellee1224@users.noreply.github.com>
2020-09-10 20:40:25 -0700

    Update ATCSettingsContactUsViewController.swift

--- xcode/Core/UIKit/Settings/ATCSettingsContactUsViewController.swift
+++ xcode/Core/UIKit/Settings/ATCSettingsContactUsViewController.swift
@@ -1,12 +1,4 @@
-//
-//  ATCSettingsContactUsViewController.swift
-//  DatingApp
-//
-//  Created by Florian Marcu on 2/2/19.
-//  Copyright © 2019 Instamobile. All rights reserved.
-//
-//
-//
+
 
 import UIKit
 
