05423006b7c2a23c32582a2dca6de5d0c054fdf0
Kunal Srivastava <kunal@masagroup.com>
2020-09-10 19:54:04 -0700

    Update ATCBarChartAdapter.swift

--- xcode/Core/Adapters/ATCBarChartAdapter.swift
+++ xcode/Core/Adapters/ATCBarChartAdapter.swift
@@ -1,10 +1,4 @@
-//
-//  ATCBarChartAdapter.swift
-//  DashboardApp
-//
-//  Created by Florian Marcu on 8/6/18.
-//  Copyright © 2018 Instamobile. All rights reserved.
-//
+
 
 import ChameleonFramework
 import Charts
