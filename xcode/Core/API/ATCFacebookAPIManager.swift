3b9a170dc30a68ab81d7093a39551a152c341023
joshtanaka <71112243+joshtanaka@users.noreply.github.com>
2020-09-10 20:05:49 -0700

    Update ATCFacebookAPIManager.swift

--- xcode/Core/API/ATCFacebookAPIManager.swift
+++ xcode/Core/API/ATCFacebookAPIManager.swift
@@ -1,9 +1,4 @@
-//
-//  ATCFacebookAPIManager.swift
-//  AppTemplatesCore
-//
-//  Created by Florian Marcu on 2/2/17.
-//  Copyright © 2017 iOS App Templates. All rights reserved.
+
 
 import FBSDKCoreKit
 import FBSDKLoginKit
