9c021d8f50fdbae640a7daa695036c29a5863185
lukemcfaul <66988084+lukemcfaul@users.noreply.github.com>
2020-09-10 20:03:21 -0700

    Update ATCSocialUserManagerProtocol.swift

--- xcode/Core/UserManagement/ATCSocialUserManagerProtocol.swift
+++ xcode/Core/UserManagement/ATCSocialUserManagerProtocol.swift
@@ -1,10 +1,4 @@
-//
-//  ATCSocialUserManagerProtocol.swift
-//  CryptoApp
-//
-//  Created by Florian Marcu on 6/29/19.
-//  Copyright © 2019 Instamobile. All rights reserved.
-//
+
 
 import UIKit
 
