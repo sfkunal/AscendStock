e95b3b973f60e2cfe6f9c5852aa69b4cd87aeb7b
lukemcfaul <66988084+lukemcfaul@users.noreply.github.com>
2020-09-10 20:08:42 -0700

    Update ATCRSSItemBaseModel.swift

--- xcode/Core/Models/ATCRSSItemBaseModel.swift
+++ xcode/Core/Models/ATCRSSItemBaseModel.swift
@@ -1,10 +1,4 @@
-//
-//  ATCRSSItemBaseModel.swift
-//  NewsReader
-//
-//  Created by Florian Marcu on 3/26/17.
-//  Copyright © 2017 iOS App Templates. All rights reserved.
-//
+
 
 import AlamofireRSSParser
 import UIKit
