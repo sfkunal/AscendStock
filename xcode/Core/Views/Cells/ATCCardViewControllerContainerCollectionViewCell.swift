d321f889bea7dfd01bbcb69b96c88ffdcba242de
Kunal Srivastava <kunal@masagroup.com>
2020-09-10 20:15:29 -0700

    Update ATCCardViewControllerContainerCollectionViewCell.swift

--- xcode/Core/Views/Cells/ATCCardViewControllerContainerCollectionViewCell.swift
+++ xcode/Core/Views/Cells/ATCCardViewControllerContainerCollectionViewCell.swift
@@ -1,10 +1,4 @@
-//
-//  ATCCardViewControllerContainerCollectionViewCell.swift
-//  FinanceApp
-//
-//  Created by Florian Marcu on 3/16/19.
-//  Copyright © 2019 Instamobile. All rights reserved.
-//
+
 
 import UIKit
 
