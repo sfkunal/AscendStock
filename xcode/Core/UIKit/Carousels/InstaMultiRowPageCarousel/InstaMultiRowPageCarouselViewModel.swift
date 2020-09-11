8e80fda6a4fd53c351c703637cc86a930b4a0b18
samuellee1224 <68315447+samuellee1224@users.noreply.github.com>
2020-09-10 20:33:02 -0700

    Update InstaMultiRowPageCarouselViewModel.swift

--- xcode/Core/UIKit/Carousels/InstaMultiRowPageCarousel/InstaMultiRowPageCarouselViewModel.swift
+++ xcode/Core/UIKit/Carousels/InstaMultiRowPageCarousel/InstaMultiRowPageCarouselViewModel.swift
@@ -1,10 +1,4 @@
-//
-//  InstaMultiRowPageCarouselViewModel.swift
-//  DatingApp
-//
-//  Created by Florian Marcu on 1/26/19.
-//  Copyright © 2019 Instamobile. All rights reserved.
-//
+
 
 import UIKit
 
