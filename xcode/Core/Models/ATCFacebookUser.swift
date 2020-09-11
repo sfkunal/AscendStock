c4558cbfa1cef1fdeffa8a1357c304a617111544
lukemcfaul <66988084+lukemcfaul@users.noreply.github.com>
2020-09-10 20:05:28 -0700

    Update ATCFacebookUser.swift

--- xcode/Core/Models/ATCFacebookUser.swift
+++ xcode/Core/Models/ATCFacebookUser.swift
@@ -1,10 +1,4 @@
-//
-//  ATCFacebookUser.swift
-//  AppTemplatesCore
-//
-//  Created by Florian Marcu on 2/2/17.
-//  Copyright © 2017 iOS App Templates. All rights reserved.
-//
+
 
 class ATCFacebookUser: ATCGenericBaseModel {
 
