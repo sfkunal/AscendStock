c2e9275570ec8603ae421903ff227b0b30464ccb
lukemcfaul <66988084+lukemcfaul@users.noreply.github.com>
2020-09-10 20:05:51 -0700

    Update ATCGoogleAd.swift

--- xcode/Core/Models/ATCGoogleAd.swift
+++ xcode/Core/Models/ATCGoogleAd.swift
@@ -1,10 +1,4 @@
-//
-//  ATCGoogleAd.swift
-//  WordpressApp
-//
-//  Created by Florian Marcu on 5/9/17.
-//  Copyright © 2017 iOS App Templates. All rights reserved.
-//
+
 
 import GoogleMobileAds
 import UIKit
